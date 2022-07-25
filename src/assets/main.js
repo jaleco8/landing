e0ac94135fde624bc4bf736e8b9c50380611adba
Jesus Leon <ing.jesusleon@gmail.com>
2022-07-25 10:15:14 -0400

    Subir Ajustes para despliegue

---
+++ src/assets/main.js
@@ -0,0 +1,66 @@
+const API = 'https://github-trending.p.rapidapi.com/repositories?language=javascript&since=daily&spoken_language_code=es';
+
+const content = null || document.getElementById('content');
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'fd2cd28105msh28273612b4095c2p162fc7jsnac1e6d219585',
+    'X-RapidAPI-Host': 'github-trending.p.rapidapi.com'
+  }
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const repositories = await fetchData(API);
+    let view = `
+    ${repositories.map(repos => `
+      <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">${repos.name}</h3>
+          <p class="mt-1 max-w-2xl text-sm text-gray-500">${repos.description}</p>
+        </div>
+        <div class="border-t border-gray-200">
+          <dl>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Autor</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">${repos.author}</dd>
+            </div>
+            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Lenguaje</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">${repos.language}</dd>
+            </div>
+            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Repositorio</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
+                  <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                    <div class="w-0 flex-1 flex items-center">
+                      <!-- Heroicon name: solid/paper-clip -->
+                      <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                      </svg>
+                    </div>
+                    <div class="ml-4 flex-shrink-0">
+                      <a href="${repos.url}" class="font-medium text-indigo-600 hover:text-indigo-500"> Código </a>
+                    </div>
+                  </li>
+                </ul>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    `).slice(0,4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (err) {
+    console.error(err);
+  }
+})();
